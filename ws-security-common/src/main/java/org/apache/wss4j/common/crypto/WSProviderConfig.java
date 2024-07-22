60ceb57c704c4cf110f02932eeb8d6a0ca021857
Freeman Fang <freeman.fang@gmail.com>
2024-07-22 17:47:11 -0400

    Revert "try not use xmlsec providers"
    This reverts commit fc269f6bac97f5f45772ef753bd710336a7b1235.

--- ws-security-common/src/main/java/org/apache/wss4j/common/crypto/WSProviderConfig.java
+++ ws-security-common/src/main/java/org/apache/wss4j/common/crypto/WSProviderConfig.java
@@ -47,7 +47,7 @@ public final class WSProviderConfig {
      * These providers, and the order in which they are added, can interfere
      * with some JVMs (such as IBMs).
      */
-    private static boolean addJceProviders = !FIPSUtils.isFIPSEnabled();
+    private static boolean addJceProviders = true;
 
     /**
      * a boolean flag to record whether we have already been statically
